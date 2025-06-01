418b6634e50bc408130a447e931dcb507ad8db3e
robbow <rob.bowland@gmail.com>
2025-06-01 21:57:39 +0100

    Improve `prompt_to_bottom` in `fish`
    `ghostty` doesn't immediately report the terminal size, leading the `tput lines` not outputting
    a sufficiently high number of lines to move the promp to the bottom.
    
    Now it'll take the max of the above or 50 - should work fine for my use case.

--- fish/functions.fish
+++ fish/functions.fish
@@ -12,7 +12,9 @@ end
 # clearing lines above the prompt if existing.
 ######################################
 function prompt_to_bottom
-    for i in (seq (tput lines))
+    set term_lines (tput lines)
+    set lines_to_clear (math "max($term_lines, 50)")
+    for i in (seq $lines_to_clear)
         echo
     end
 end
