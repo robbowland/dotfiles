8894412d5237796a78ccd6dbf5696489f760b5a2
robbowland <me@robbow.land>
2025-11-02 13:20:38 +0000

    Quote `fzf` aliased functions

--- fzf/fish/aliases.fish
+++ fzf/fish/aliases.fish
@@ -1,5 +1,5 @@
 # -- perform (g)it (ch)eckout using fzf
-alias gch=fzf_git_checkout
+alias gch='fzf_git_checkout'
 
 # -- perform (g)it (sh)ow using fzf
-alias gsh=fzf_git_show_commit
+alias gsh='fzf_git_show_commit'
