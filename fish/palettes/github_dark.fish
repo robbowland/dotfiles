d34be12a5160e91379f72a3ace3dd780d8603915
robbowland <me@robbow.land>
2025-11-10 13:34:40 +0000

    Update `fish` palette

--- fish/palettes/github_dark.fish
+++ fish/palettes/github_dark.fish
@@ -1,17 +1,28 @@
 #!/bin/fish
 
 # Palette: Github Dark
-export PALETTE_BLACK="#000000"
-export PALETTE_WHITE="#ffffff"
-export PALETTE_GRAY="#484f58"
-export PALETTE_GRAY_DARK="#303030"
-export PALETTE_GRAY_DARKER="#101010"
-export PALETTE_GRAY_DARKEST="#080808"
-export PALETTE_BLUE="#58a6ff"
-export PALETTE_CYAN="#79c0ff"
-export PALETTE_GREEN="#3fb950"
-export PALETTE_RED="#f85149"
-export PALETTE_YELLOW="#d29922"
-export PALETTE_ORANGE="#db6d28"
-export PALETTE_MAGENTA="#bc8cff"
-export PALETTE_PINK="#db61a2"
+# --- Palette (bright) ---
+set -gx PALETTE_WHITE '#e6edf3'
+set -gx PALETTE_CYAN '#79c0ff'
+set -gx PALETTE_GREEN '#3fb950'
+set -gx PALETTE_YELLOW '#d29922'
+set -gx PALETTE_MAGENTA '#bc8cff'
+set -gx PALETTE_BLUE '#58a6ff'
+set -gx PALETTE_ORANGE '#db6d28'
+set -gx PALETTE_PINK '#db61a2'
+set -gx PALETTE_RED '#f85149'
+set -gx PALETTE_GRAY "#484f58"
+set -gx PALETTE_GRAY_DARK "#303030"
+set -gx PALETTE_GRAY_DARKER "#101010"
+set -gx PALETTE_GRAY_DARKEST "#080808"
+
+# --- Palette (dim variants; same hue, lower luminance/sat) ---
+set -gx PALETTE_WHITE_DIM '#b7c2cc'
+set -gx PALETTE_CYAN_DIM '#4e99d4'
+set -gx PALETTE_GREEN_DIM '#2f9142'
+set -gx PALETTE_YELLOW_DIM '#9d731a'
+set -gx PALETTE_MAGENTA_DIM '#946ccc'
+set -gx PALETTE_BLUE_DIM '#3e7cc4'
+set -gx PALETTE_ORANGE_DIM '#aa541e'
+set -gx PALETTE_PINK_DIM '#ad4d85'
+set -gx PALETTE_RED_DIM '#bd3d38'
