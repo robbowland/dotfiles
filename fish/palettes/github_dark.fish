bbb31d6e6d345ef2da5d75c858d9c96f70b58d29
robbowland <me@robbow.land>
2025-11-11 14:11:48 +0000

    Update palette colours

--- fish/palettes/github_dark.fish
+++ fish/palettes/github_dark.fish
@@ -1,28 +1,63 @@
 #!/bin/fish
 
-# Palette: Github Dark
-# --- Palette (bright) ---
-set -gx PALETTE_WHITE '#e6edf3'
-set -gx PALETTE_CYAN '#79c0ff'
-set -gx PALETTE_GREEN '#3fb950'
-set -gx PALETTE_YELLOW '#d29922'
-set -gx PALETTE_MAGENTA '#bc8cff'
-set -gx PALETTE_BLUE '#58a6ff'
-set -gx PALETTE_ORANGE '#db6d28'
-set -gx PALETTE_PINK '#db61a2'
-set -gx PALETTE_RED '#f85149'
-set -gx PALETTE_GRAY "#484f58"
-set -gx PALETTE_GRAY_DARK "#303030"
-set -gx PALETTE_GRAY_DARKER "#101010"
-set -gx PALETTE_GRAY_DARKEST "#080808"
+# Palette: GitHub Dark
 
-# --- Palette (dim variants; same hue, lower luminance/sat) ---
+#-- deep neutrals
+set -gx PALETTE_DARK '#101010'
+set -gx PALETTE_DARKEST '#080808'
+set -gx PALETTE_BLACK '#000000'
+
+#-- surfaces (microcontrast)
+set -gx PALETTE_SURFACE_0 '#080808' # background
+set -gx PALETTE_SURFACE_1 '#0c0c0c' # subtle selection
+set -gx PALETTE_SURFACE_2 '#1a1a1a' # hover / active
+
+#-- white
+set -gx PALETTE_WHITE_BRIGHT '#ffffff'
+set -gx PALETTE_WHITE '#e6edf3'
 set -gx PALETTE_WHITE_DIM '#b7c2cc'
-set -gx PALETTE_CYAN_DIM '#4e99d4'
-set -gx PALETTE_GREEN_DIM '#2f9142'
-set -gx PALETTE_YELLOW_DIM '#9d731a'
-set -gx PALETTE_MAGENTA_DIM '#946ccc'
+
+#-- gray
+set -gx PALETTE_GRAY_BRIGHT '#6e7681'
+set -gx PALETTE_GRAY '#484f58'
+set -gx PALETTE_GRAY_DIM '#303030'
+
+#-- blue
+set -gx PALETTE_BLUE_BRIGHT '#79c0ff'
+set -gx PALETTE_BLUE '#58a6ff'
 set -gx PALETTE_BLUE_DIM '#3e7cc4'
-set -gx PALETTE_ORANGE_DIM '#aa541e'
+
+#-- cyan
+set -gx PALETTE_CYAN_BRIGHT '#a5d6ff'
+set -gx PALETTE_CYAN '#79c0ff'
+set -gx PALETTE_CYAN_DIM '#4e99d4'
+
+#-- green
+set -gx PALETTE_GREEN_BRIGHT '#56d364'
+set -gx PALETTE_GREEN '#3fb950'
+set -gx PALETTE_GREEN_DIM '#2f9142'
+
+#-- yellow
+set -gx PALETTE_YELLOW_BRIGHT '#e3b341'
+set -gx PALETTE_YELLOW '#d29922'
+set -gx PALETTE_YELLOW_DIM '#9d731a'
+
+#-- magenta
+set -gx PALETTE_MAGENTA_BRIGHT '#d2a8ff'
+set -gx PALETTE_MAGENTA '#bc8cff'
+set -gx PALETTE_MAGENTA_DIM '#946ccc'
+
+#-- pink
+set -gx PALETTE_PINK_BRIGHT '#ff8dc7'
+set -gx PALETTE_PINK '#db61a2'
 set -gx PALETTE_PINK_DIM '#ad4d85'
+
+#-- orange
+set -gx PALETTE_ORANGE_BRIGHT '#ff934a'
+set -gx PALETTE_ORANGE '#db6d28'
+set -gx PALETTE_ORANGE_DIM '#aa541e'
+
+#-- red
+set -gx PALETTE_RED_BRIGHT '#ff8182'
+set -gx PALETTE_RED '#f85149'
 set -gx PALETTE_RED_DIM '#bd3d38'
