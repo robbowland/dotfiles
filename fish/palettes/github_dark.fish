35fb01366345a4d50a68fd67d4c42e799c1334ab
robbowland <me@robbow.land>
2025-11-11 23:42:46 +0000

    Update a bunch of theme stuff

--- fish/palettes/github_dark.fish
+++ fish/palettes/github_dark.fish
@@ -2,62 +2,62 @@
 
 # Palette: GitHub Dark
 
-#-- deep neutrals
-set -gx PALETTE_DARK '#101010'
-set -gx PALETTE_DARKEST '#080808'
+# -- black
+set -gx PALETTE_BLACK_BRIGHT '#161b22'
 set -gx PALETTE_BLACK '#000000'
+set -gx PALETTE_BLACK_DIM '#0d1117'
 
-#-- surfaces (microcontrast)
-set -gx PALETTE_SURFACE_0 '#0c0c0c' # new background
-set -gx PALETTE_SURFACE_1 '#101010' # subtle selection
-set -gx PALETTE_SURFACE_2 '#181818' # hover / active
-
-#-- white
+# -- white
 set -gx PALETTE_WHITE_BRIGHT '#ffffff'
 set -gx PALETTE_WHITE '#e6edf3'
-set -gx PALETTE_WHITE_DIM '#b7c2cc'
+set -gx PALETTE_WHITE_DIM '#b1bac4'
 
-#-- gray
-set -gx PALETTE_GRAY_BRIGHT '#6e7681'
-set -gx PALETTE_GRAY '#484f58'
-set -gx PALETTE_GRAY_DIM '#303030'
+# -- gray
+set -gx PALETTE_GRAY_BRIGHT '#8b949e'
+set -gx PALETTE_GRAY '#6e7681'
+set -gx PALETTE_GRAY_DIM '#484f58'
 
-#-- blue
-set -gx PALETTE_BLUE_BRIGHT '#79c0ff'
-set -gx PALETTE_BLUE '#58a6ff'
-set -gx PALETTE_BLUE_DIM '#3e7cc4'
+# -- surfaces (micro-contrast plates)
+set -gx PALETTE_SURFACE_0 '#0c0c0c'
+set -gx PALETTE_SURFACE_1 '#101010'
+set -gx PALETTE_SURFACE_2 '#181818'
 
-#-- cyan
-set -gx PALETTE_CYAN_BRIGHT '#a5d6ff'
-set -gx PALETTE_CYAN '#79c0ff'
-set -gx PALETTE_CYAN_DIM '#4e99d4'
+# -- blue
+set -gx PALETTE_BLUE_BRIGHT '#a5d6ff'
+set -gx PALETTE_BLUE '#79c0ff'
+set -gx PALETTE_BLUE_DIM '#58a6ff'
 
-#-- green
+# -- cyan
+set -gx PALETTE_CYAN_BRIGHT '#d2f8ff'
+set -gx PALETTE_CYAN '#a3e8f2'
+set -gx PALETTE_CYAN_DIM '#6ec1cc'
+
+# -- green
 set -gx PALETTE_GREEN_BRIGHT '#56d364'
 set -gx PALETTE_GREEN '#3fb950'
-set -gx PALETTE_GREEN_DIM '#2f9142'
+set -gx PALETTE_GREEN_DIM '#2ea043'
 
-#-- yellow
-set -gx PALETTE_YELLOW_BRIGHT '#e3b341'
-set -gx PALETTE_YELLOW '#d29922'
-set -gx PALETTE_YELLOW_DIM '#9d731a'
+# -- yellow
+set -gx PALETTE_YELLOW_BRIGHT '#f2cc60'
+set -gx PALETTE_YELLOW '#e3b341'
+set -gx PALETTE_YELLOW_DIM '#d29922'
 
-#-- magenta
+# -- magenta
 set -gx PALETTE_MAGENTA_BRIGHT '#d2a8ff'
 set -gx PALETTE_MAGENTA '#bc8cff'
-set -gx PALETTE_MAGENTA_DIM '#946ccc'
+set -gx PALETTE_MAGENTA_DIM '#a371f7'
 
-#-- pink
-set -gx PALETTE_PINK_BRIGHT '#ff8dc7'
+# -- orange (already tuned earlier; keep)
+set -gx PALETTE_ORANGE_BRIGHT '#ffa657'
+set -gx PALETTE_ORANGE '#f0883e'
+set -gx PALETTE_ORANGE_DIM '#db6d28'
+
+# -- pink
+set -gx PALETTE_PINK_BRIGHT '#ff9bce'
 set -gx PALETTE_PINK '#db61a2'
-set -gx PALETTE_PINK_DIM '#ad4d85'
+set -gx PALETTE_PINK_DIM '#bf4b8a'
 
-#-- orange
-set -gx PALETTE_ORANGE_BRIGHT '#ff934a'
-set -gx PALETTE_ORANGE '#db6d28'
-set -gx PALETTE_ORANGE_DIM '#aa541e'
-
-#-- red
-set -gx PALETTE_RED_BRIGHT '#ff8182'
-set -gx PALETTE_RED '#f85149'
-set -gx PALETTE_RED_DIM '#bd3d38'
+# -- red
+set -gx PALETTE_RED_BRIGHT '#ffa198'
+set -gx PALETTE_RED '#ff7b72'
+set -gx PALETTE_RED_DIM '#da3633'
