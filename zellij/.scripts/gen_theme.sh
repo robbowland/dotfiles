4500161bd2b26ef78f5d91544f2d34f164ab5e95
robbowland <me@robbow.land>
2025-11-14 21:46:35 +0000

    Update `zellij` themes

---
+++ zellij/.scripts/gen_theme.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+BASE="${XDG_CONFIG_HOME:-$HOME/.config}"
+MOD_DIR="$BASE/zellij"
+GEN="$BASE/.scripts/theme/gen_theme.sh"
+
+render() {
+	IN="$1" OUT="$2" "$GEN"
+}
+
+render "$MOD_DIR/config.kdl.in" "$MOD_DIR/config.kdl"
+render "$MOD_DIR/layouts/default.kdl.in" "$MOD_DIR/layouts/default.kdl"
